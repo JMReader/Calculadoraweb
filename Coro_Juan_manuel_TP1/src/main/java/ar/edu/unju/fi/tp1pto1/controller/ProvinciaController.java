f0f9e98229314a8ecc9affdc0a4c6608a8818219
Jose Luis <Jose Luis@DESKTOP-2E8Q40F>
2021-04-09 17:51:16 -0300

    primer punto

---
+++ Coro_Juan_manuel_TP1/src/main/java/ar/edu/unju/fi/tp1pto1/controller/ProvinciaController.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.tp1pto1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.edu.unju.tp1.model.Provincia;
+
+@Controller
+class ProvinciaController {
+
+	//Provincia unaProvincia = new Provincia(); //instanciando la clase provincia, estamos creando una variable una provincia que tiene un objeto asignando a un espacio de memoria
+	@Autowired
+	  Provincia unaProvincia;
+	  
+	@GetMapping({"/provincia"})
+		public String cargarProvincia(Model model) {
+			unaProvincia.setNombre("Salta Argentina");
+			model.addAttribute("Nicole", unaProvincia);
+			return "provincia";
+		}
+}
